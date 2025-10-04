f5a3350a9da4099392a3356f555527cf2b0c0b29
DorianABDS <abbadessadorian@gmail.com>
2025-10-05 00:00:46 +0200

    feat(App): implement main application structure with section components and background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+import Overlay from './components/Overlay';
+import ScrollContainer from './components/ScrollContainer';
+import HeroSection from './components/sections/HeroSection';
+import AboutSection from './components/sections/AboutSection';
+import ProjectSection from './components/sections/ProjectSection';
+import ContactSection from './components/sections/ContactSection';
+import useScrollSection from './hooks/useScrollSection';
+import bgImage from "./assets/img/bg-pf.webp";
+
 export default function App() {
+  const totalSections = 4;
+  const { currentSection, setCurrentSection } = useScrollSection(totalSections);
+
   return (
-    <h1 className="text-red-500 text-4xl font-bold">
-      Hello Tailwind v4!
-    </h1>
+    <div className="h-screen overflow-hidden bg-black">
+      <Overlay />
+
+      <ScrollContainer currentSection={currentSection}>
+        <HeroSection
+          bgImage={bgImage}
+          title="HeroSection"
+        />
+
+        <AboutSection title="AboutSection" />
+
+        <ProjectSection title="ProjectSection" />
+
+        <ContactSection title="ContactSection" />
+      </ScrollContainer>
+    </div>
   );
 }
